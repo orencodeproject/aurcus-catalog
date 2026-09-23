5f8f0a84f89425fb26dcc7a61d74e3da3c533db6
orencodeproject <orencodeproject@gmail.com>
2026-09-24 00:59:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@
       Kurs IDR -> USD (perkiraan, per akhir September 2026).
       Update angka ini kalau kurs berubah signifikan.
     */
-    USD_RATE: 16500,
+    USD_RATE: 16000,
 
     /*
       Markup konversi USD = 20% dari hasil konversi IDR -> USD.
